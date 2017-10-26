6c8719f958db7e1e2409a51c296db9c1ebe60c9f
Darko Bozhinovski <dbozhinovski@gmail.com>
2017-10-27 00:24:04 +0200

    Adds intl support for addNewService (sans translations)

--- src/components/services/tabs/Tabbar.js
+++ src/components/services/tabs/Tabbar.js
@@ -54,6 +54,8 @@ export default class TabBar extends Component {
       deleteService,
     } = this.props;
 
+    const { intl } = this.context;
+
     return (
       <div>
         <TabBarSortableList
@@ -70,6 +72,7 @@ export default class TabBar extends Component {
           axis="y"
           lockAxis="y"
           helperClass="is-reordering"
+          intl={intl}
         />
       </div>
     );
