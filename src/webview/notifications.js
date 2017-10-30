9593b28a7b2c3aa4adb6ccca5d4d2d5bf0b95017
Stefan Malzner <stefan@adlk.io>
2017-10-30 22:52:03 +0100

    Recipe Notification API refactor
    Fixes: #104

--- src/webview/notifications.js
+++ src/webview/notifications.js
@@ -1,47 +1,46 @@
 const { ipcRenderer } = require('electron');
 const uuidV1 = require('uuid/v1');
-// const FranzNotificationStore = [];
 
 class Notification {
+  static permission = 'granted';
+
   constructor(title = '', options = {}) {
     this.title = title;
     this.options = options;
     this.notificationId = uuidV1();
-    this.onclick = () => { };
 
-    ipcRenderer.sendToHost('notification', Notification.onNotify({
+    ipcRenderer.sendToHost('notification', this.onNotify({
       notificationId: this.notificationId,
       title,
       options,
     }));
 
-    ipcRenderer.on(`notification-onclick:${this.notificationId}`, () => {
+    ipcRenderer.once(`notification-onclick:${this.notificationId}`, () => {
       this.onclick();
     });
   }
+
+  static requestPermission(cb = null) {
+    if (!cb) {
+      return new Promise((resolve) => {
+        resolve(Notification.permission);
+      });
+    }
+
+    if (typeof (cb) === 'function') {
+      return cb(Notification.permission);
+    }
+
+    return Notification.permission;
+  }
+
+  onNotify(data) {
+    return data;
+  }
+
+  onClick() {}
+
+  close() {}
 }
-
-Notification.permission = 'granted';
-
-Notification.requestPermission = (cb = null) => {
-  console.log(this);
-  if (!cb) {
-    return new Promise((resolve) => {
-      resolve(Notification.permission);
-    });
-  }
-
-  if (typeof (cb) === 'function') {
-    return cb(Notification.permission);
-  }
-
-  return Notification.permission;
-};
-
-Notification.close = () => {
-  // no implementation yet
-};
-
-Notification.onNotify = data => data;
 
 window.Notification = Notification;
