d83eed53a0cd5a5e705dcd8b28092bf2c4509593
Stefan Malzner <stefan@adlk.io>
2017-12-22 11:50:02 +0100

    add deebug output

--- src/electron/deepLinking.js
+++ src/electron/deepLinking.js
@@ -1,6 +1,8 @@
 export default function handleDeepLink(window, rawUrl) {
   const url = rawUrl.replace('franz://', '');
 
+  console.log('handleDeepLink', url);
+
   if (!url) return;
 
   window.webContents.send('navigateFromDeepLink', { url });
