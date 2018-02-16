656e2f422d1b7d690686dcb98542a42d648ea22d
haraldox <hnaumann+github@gmail.com>
2018-02-16 10:26:49 +0100

    trying to get to the bottom of this

--- src/lib/Menu.js
+++ src/lib/Menu.js
@@ -22,10 +22,14 @@ const template = [
         role: 'cut',
       },
       {
-        role: 'copy',
+        label: 'Copy',
+        accelerator: 'Cmd+C',
+        selector: 'copy:',
       },
       {
-        role: 'paste',
+        label: 'Paste',
+        accelerator: 'Cmd+V',
+        selector: 'paste:',
       },
       {
         role: 'pasteandmatchstyle',
