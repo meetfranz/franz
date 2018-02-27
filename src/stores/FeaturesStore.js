b6bda41e25e06dfb590753c8fe19f8cc1930d0c1
haraldox <hnaumann+github@gmail.com>
2018-02-27 13:41:34 +0100

    REMOVE `base` getter

--- src/stores/FeaturesStore.js
+++ src/stores/FeaturesStore.js
@@ -1,10 +1,9 @@
-import { action, computed, observable } from 'mobx';
+import { computed, observable } from 'mobx';
 
 import Store from './lib/Store';
 import CachedRequest from './lib/CachedRequest';
-import Request from './lib/Request';
 
-export default class RecipesStore extends Store {
+export default class FeaturesStore extends Store {
   @observable baseFeaturesRequest = new CachedRequest(this.api.features, 'base');
   @observable featuresRequest = new CachedRequest(this.api.features, 'features');
 
@@ -15,29 +14,23 @@ export default class RecipesStore extends Store {
     ]);
   }
 
-  @computed get base() {
-    return this.baseFeaturesRequest.execute().result || {};
-  }
-
   @computed get features() {
     if (this.stores.user.isLoggedIn) {
       return this.featuresRequest.execute().result || {};
     }
 
-    return this.base;
+    return this.baseFeaturesRequest.execute().result || {};
   }
 
   _debugFeatures() {
-    console.log(this.base, this.features)
+    console.log(this.features);
   }
 
   _monitorLoginStatus() {
     if (this.stores.user.isLoggedIn) {
       this.featuresRequest.invalidate({ immediately: true });
-      this.featuresRequest.execute();
     } else {
       this.baseFeaturesRequest.invalidate({ immediately: true });
-      this.baseFeaturesRequest.execute();
     }
   }
 }
