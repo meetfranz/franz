8978e0caa00959e54af05620db765cd95b30e73d
Stefan Malzner <stefan@adlk.io>
2018-03-19 13:17:19 +0100

    fix logic issue

--- src/components/layout/AppLayout.js
+++ src/components/layout/AppLayout.js
@@ -7,7 +7,7 @@ import { TitleBar } from 'electron-react-titlebar';
 import InfoBar from '../ui/InfoBar';
 import globalMessages from '../../i18n/globalMessages';
 
-import { isMac, isLinux } from '../../environment';
+import { isWindows } from '../../environment';
 
 function createMarkup(HTMLString) {
   return { __html: HTMLString };
@@ -90,7 +90,7 @@ export default class AppLayout extends Component {
     return (
       <div>
         <div className="app">
-          {!isMac || !isLinux && <TitleBar menu={window.franz.menu.template} icon={'assets/images/logo.svg'} />}
+          {isWindows && <TitleBar menu={window.franz.menu.template} icon={'assets/images/logo.svg'} />}
           <div className="app__content">
             {sidebar}
             <div className="app__service">
