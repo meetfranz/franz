e3975b5051e0ba7fbc77e65afb82d72a45b06546
Stefan Malzner <stefan@adlk.io>
2018-12-08 23:43:52 +0100

    Make spinner work in lightMode

--- src/components/ui/FullscreenLoader/index.js
+++ src/components/ui/FullscreenLoader/index.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import injectSheet from 'react-jss';
 import classnames from 'classnames';
+import { inject } from 'mobx-react';
 
 import Loader from '../Loader';
 
 import styles from './styles';
 
-export default injectSheet(styles)(({ classes, className, title, children }) => (
-  <div className={classes.wrapper}>
-    <div
-      className={classnames({
-        [`${classes.component}`]: true,
-        [`${className}`]: className,
-      })}
-    >
-      <h1 className={classes.title}>{title}</h1>
-      <Loader color="#FFF" />
-      {children && (
-        <div className={classes.content}>
-          {children}
-        </div>
-      )}
+export default inject('stores')(injectSheet(styles)(({ stores, classes, className, title, children }) => (
+    <div className={classes.wrapper}>
+      <div
+        className={classnames({
+          [`${classes.component}`]: true,
+          [`${className}`]: className,
+        })}
+      >
+        <h1 className={classes.title}>{title}</h1>
+        <Loader color={stores.settings.app.darkMode ? '#FFF' : '#000'} />
+        {children && (
+          <div className={classes.content}>
+            {children}
+          </div>
+        )}
+      </div>
     </div>
-  </div>
-));
+  )
+  ));
