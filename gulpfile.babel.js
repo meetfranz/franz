73308038ab9fd2e04973b45a0ed250a42bb543ad
Stefan Malzner <stefan@adlk.io>
2018-12-12 11:10:37 +0100

    Simplify dictionary step

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,6 +25,7 @@ const paths = {
   src: 'src',
   dest: 'build',
   tmp: '.tmp',
+  dictionaries: 'build/dictionaries',
   package: `out/${config.version}`,
   html: {
     src: 'src/**/*.html',
@@ -132,13 +133,30 @@ export function webserver() {
     }));
 }
 
+export function dictionaries(done) {
+  const { SPELLCHECKER_LOCALES } = require('./build/i18n/languages');
+
+  let packages = '';
+  Object.keys(SPELLCHECKER_LOCALES).forEach((key) => { packages = `${packages} hunspell-dict-${key}`; });
+
+  _shell(`
+    rm -rf ${paths.dictionaries}
+    npm install --prefix ${paths.dictionaries} ${packages}
+    mv ${paths.dictionaries}/node_modules/* ${paths.dictionaries}
+    rm -rf ${paths.dictionaries}/node_modules ${paths.dictionaries}/package-lock.json`,
+    done,
+  );
+} 
+
 export function sign(done) {
   _shell(`codesign --verbose=4 --deep --strict --force --sign "${process.env.SIGNING_IDENTITY}" "${__dirname}/node_modules/electron/dist/Electron.app"`, done);
 }
 
 const build = gulp.series(
+  clean,
   gulp.parallel(mvSrc, mvPackageJson),
   gulp.parallel(html, scripts, styles),
+  dictionaries,
 );
 export { build };
 
