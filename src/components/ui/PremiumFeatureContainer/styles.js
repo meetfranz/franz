3a0a0f136cab3cb46b9a3a62894b952f86588026
Stefan Malzner <stefan@adlk.io>
2018-12-12 21:03:14 +0100

    Ignore clicks on premium elements

--- src/components/ui/PremiumFeatureContainer/styles.js
+++ src/components/ui/PremiumFeatureContainer/styles.js
@@ -5,6 +5,7 @@ export default theme => ({
     margin: [0, 0, 20, -20],
     padding: 20,
     'border-radius': theme.borderRadius,
+    pointerEvents: 'none',
   },
   titleContainer: {
     display: 'flex',
@@ -20,6 +21,7 @@ export default theme => ({
     'border-radius': theme.borderRadiusSmall,
     padding: [2, 4],
     'font-size': 12,
+    pointerEvents: 'initial',
   },
   content: {
     opacity: 0.5,
