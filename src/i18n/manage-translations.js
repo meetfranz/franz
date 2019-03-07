d12b419b216b030d063b49e6d149c5110d4fec99
Dominik Guzei <dominik.guzei@gmail.com>
2019-03-07 14:37:44 +0100

    only manage en-US translations

--- src/i18n/manage-translations.js
+++ src/i18n/manage-translations.js
@@ -1,10 +1,9 @@
 require('@babel/register');
 const manageTranslations = require('react-intl-translations-manager').default;
-// const { APP_LOCALES } = require('../src/i18n/languages');
 
 manageTranslations({
   messagesDirectory: 'src/i18n/messages',
   translationsDirectory: 'src/i18n/locales',
   singleMessagesFile: true,
-  languages: ['en-US'], // Object.keys(APP_LOCALES),
+  languages: ['en-US'],
 });
