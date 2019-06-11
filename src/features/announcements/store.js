43a90e17e224afe09f5877e672a05717a61f442c
Stefan Malzner <stefan@adlk.io>
2019-06-11 21:08:14 +0200

    fix default locale language fallback

--- src/features/announcements/store.js
+++ src/features/announcements/store.js
@@ -14,6 +14,7 @@ import { createActionBindings } from '../utils/ActionBinding';
 import { createReactions } from '../../stores/lib/Reaction';
 import { gaEvent } from '../../lib/analytics';
 import { matchRoute } from '../../helpers/routing-helpers';
+import { DEFAULT_APP_SETTINGS } from '../../config';
 
 const LOCAL_STORAGE_KEY = 'announcements';
 
@@ -30,12 +31,12 @@ export class AnnouncementsStore extends FeatureStore {
 
   @computed get announcement() {
     if (!this.stores || !getAnnouncementRequest.result) return null;
-    const { locale, defaultLocale } = this.stores.app;
+    const { locale } = this.stores.app;
     const announcement = getAnnouncementRequest.result;
     // User locale
     if (announcement[locale]) return announcement[locale];
     // Default locale
-    if (announcement[defaultLocale]) return announcement[defaultLocale];
+    if (announcement[DEFAULT_APP_SETTINGS.fallbackLocale]) return announcement[DEFAULT_APP_SETTINGS.fallbackLocale];
     // No locales specified
     return announcement;
   }
