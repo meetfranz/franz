2469be543048b575c0cd8e8fccdef2a73e6046d3
Stefan Malzner <stefan@adlk.io>
2019-10-17 14:57:57 +0200

    Add debugging logs

--- src/webview/lib/RecipeWebview.js
+++ src/webview/lib/RecipeWebview.js
@@ -2,6 +2,8 @@
 const { ipcRenderer } = require('electron');
 const fs = require('fs-extra');
 
+const debug = require('debug')('Franz:Plugin:RecipeWebview');
+
 class RecipeWebview {
   constructor() {
     this.countCache = {
@@ -11,6 +13,8 @@ class RecipeWebview {
 
     ipcRenderer.on('poll', () => {
       this.loopFunc();
+
+      debug('Poll event');
     });
   }
 
@@ -44,8 +48,11 @@ class RecipeWebview {
       indirect: indirect > 0 ? indirect : 0,
     };
 
+
     ipcRenderer.sendToHost('messages', count);
     Object.assign(this.countCache, count);
+
+    debug('Sending badge count to host', count);
   }
 
   /**
@@ -61,6 +68,8 @@ class RecipeWebview {
       styles.innerHTML = data.toString();
 
       document.querySelector('head').appendChild(styles);
+
+      debug('Append styles', styles);
     });
   }
 
