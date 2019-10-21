bd4c19826e936667962f410c9957b6259ebe8c06
Stefan Malzner <stefan@adlk.io>
2019-10-21 13:29:55 +0200

    reload app after sleep with 2s waiting time

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -207,10 +207,9 @@ export default class AppStore extends Store {
       if (this.timeSuspensionStart.add(10, 'm').isBefore(moment())) {
         debug('Reloading services, user info and features');
 
-        this.actions.service.reloadAll();
-
-        this.stores.user.getUserInfoRequest.invalidate({ immediately: true });
-        this.stores.features.featuresRequest.invalidate({ immediately: true });
+        setTimeout(() => {
+          window.location.reload();
+        }, ms('2s'));
 
         statsEvent('resumed-app');
       }
