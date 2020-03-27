a8dea57b39e7b0a03695225c6e4c998120fc044e
Stefan Malzner <stefan@adlk.io>
2020-03-27 11:01:26 +0100

    fix(Service): Fix issue with user agent override in service workers

---
+++ src/electron/userAgent.js
@@ -0,0 +1,35 @@
+
+import os from 'os';
+import macosVersion from 'macos-version';
+import { isMac, isWindows } from '../environment';
+
+function macOS() {
+  const version = macosVersion();
+
+  return `Macintosh; Intel Mac OS X ${version.replace('.', '_')}`;
+}
+
+function windows() {
+  const version = os.release();
+  const [majorVersion, minorVersion] = version.split('.');
+  return `Windows NT ${majorVersion}.${minorVersion}; Win64; x64`;
+}
+
+function linux() {
+  return 'X11; Ubuntu; Linux x86_64';
+}
+
+export default function userAgent() {
+  let platformString = '';
+
+  if (isMac) {
+    platformString = macOS();
+  } else if (isWindows) {
+    platformString = windows();
+  } else {
+    platformString = linux();
+  }
+
+  // TODO: Update AppleWebKit and Safari version after electron update
+  return `Mozilla/5.0 (${platformString}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${process.versions.chrome} Safari/537.36`;
+}
