ae2272dd9945784c32cf12f9903fed4658ccc71e
Stefan Malzner <stefan@adlk.io>
2020-07-15 10:31:20 +0200

    fix(App): Fix prop validation in feature list

--- src/components/ui/FeatureList.js
+++ src/components/ui/FeatureList.js
@@ -76,7 +76,7 @@ export class FeatureList extends Component {
   static propTypes = {
     className: PropTypes.string,
     featureClassName: PropTypes.string,
-    plan: PropTypes.oneOf(PLANS),
+    plan: PropTypes.oneOf(Object.keys(PLANS)),
   };
 
   static defaultProps = {
