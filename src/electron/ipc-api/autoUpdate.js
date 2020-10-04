802f01be888dd562cad4afe6eddcb28e48979f8d
Stefan Malzner <stefan@adlk.io>
2020-10-04 20:20:31 +0200

    Revert "test with disabled autoupdater"
    This reverts commit 5a56bea47075c02dae9ca2318a2bedfe5e44b046.

--- src/electron/ipc-api/autoUpdate.js
+++ src/electron/ipc-api/autoUpdate.js
@@ -1,60 +1,60 @@
-// import { app, ipcMain } from 'electron';
-// import { autoUpdater } from 'electron-updater';
+import { app, ipcMain } from 'electron';
+import { autoUpdater } from 'electron-updater';
 
-// const debug = require('debug')('Franz:ipcApi:autoUpdate');
+const debug = require('debug')('Franz:ipcApi:autoUpdate');
 
-export default () => {
-  // if (process.platform === 'darwin' || process.platform === 'win32' || process.env.APPIMAGE) {
-  //   ipcMain.on('autoUpdate', (event, args) => {
-  //     try {
-  //       autoUpdater.autoInstallOnAppQuit = false;
-  //       autoUpdater.allowPrerelease = Boolean(params.settings.app.get('beta'));
-  //       if (args.action === 'check') {
-  //         autoUpdater.checkForUpdates();
-  //       } else if (args.action === 'install') {
-  //         debug('install update');
-  //         autoUpdater.quitAndInstall();
-  //         // we need to send a quit event
-  //         setTimeout(() => {
-  //           app.quit();
-  //         }, 20);
-  //       }
-  //     } catch (e) {
-  //       console.log('debugging test');
-  //       console.log(e);
-  //       // params.mainWindow.webContents.send('autoUpdate', { error: true });
-  //     }
-  //   });
+export default (params) => {
+  if (process.platform === 'darwin' || process.platform === 'win32' || process.env.APPIMAGE) {
+    ipcMain.on('autoUpdate', (event, args) => {
+      try {
+        autoUpdater.autoInstallOnAppQuit = false;
+        autoUpdater.allowPrerelease = Boolean(params.settings.app.get('beta'));
+        if (args.action === 'check') {
+          autoUpdater.checkForUpdates();
+        } else if (args.action === 'install') {
+          debug('install update');
+          autoUpdater.quitAndInstall();
+          // we need to send a quit event
+          setTimeout(() => {
+            app.quit();
+          }, 20);
+        }
+      } catch (e) {
+        console.log('debugging test');
+        console.log(e);
+        // params.mainWindow.webContents.send('autoUpdate', { error: true });
+      }
+    });
 
-  //   autoUpdater.on('update-not-available', () => {
-  //     debug('update-not-available');
-  //     params.mainWindow.webContents.send('autoUpdate', { available: false });
-  //   });
+    autoUpdater.on('update-not-available', () => {
+      debug('update-not-available');
+      params.mainWindow.webContents.send('autoUpdate', { available: false });
+    });
 
-  //   autoUpdater.on('update-available', (event) => {
-  //     debug('update-available');
-  //     params.mainWindow.webContents.send('autoUpdate', {
-  //       version: event.version,
-  //       available: true,
-  //     });
-  //   });
+    autoUpdater.on('update-available', (event) => {
+      debug('update-available');
+      params.mainWindow.webContents.send('autoUpdate', {
+        version: event.version,
+        available: true,
+      });
+    });
 
-  //   autoUpdater.on('download-progress', (progressObj) => {
-  //     let logMessage = `Download speed: ${progressObj.bytesPerSecond}`;
-  //     logMessage = `${logMessage} - Downloaded ${progressObj.percent}%`;
-  //     logMessage = `${logMessage} (${progressObj.transferred}/${progressObj.total})`;
+    autoUpdater.on('download-progress', (progressObj) => {
+      let logMessage = `Download speed: ${progressObj.bytesPerSecond}`;
+      logMessage = `${logMessage} - Downloaded ${progressObj.percent}%`;
+      logMessage = `${logMessage} (${progressObj.transferred}/${progressObj.total})`;
 
-  //     debug(logMessage);
-  //   });
+      debug(logMessage);
+    });
 
-  //   autoUpdater.on('update-downloaded', () => {
-  //     debug('update-downloaded');
-  //     params.mainWindow.webContents.send('autoUpdate', { downloaded: true });
-  //   });
+    autoUpdater.on('update-downloaded', () => {
+      debug('update-downloaded');
+      params.mainWindow.webContents.send('autoUpdate', { downloaded: true });
+    });
 
-  //   autoUpdater.on('error', () => {
-  //     debug('update-error');
-  //     params.mainWindow.webContents.send('autoUpdate', { error: true });
-  //   });
-  // }
+    autoUpdater.on('error', () => {
+      debug('update-error');
+      params.mainWindow.webContents.send('autoUpdate', { error: true });
+    });
+  }
 };
