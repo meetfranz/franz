15b7d3454db47c7edb27243d850c2b31a87aaf6a
Stefan Malzner <stefan@adlk.io>
2021-12-14 12:33:32 +0100

    fix matomo integration

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,44 +1,43 @@
 import { app } from '@electron/remote';
+import ElectronCookies from '@meetfranz/electron-cookies';
 // import querystring from 'querystring';
 
 // import { STATS_API } from '../config';
 // import { isDevMode, GA_ID } from '../environment';
 
+ElectronCookies.enable({
+  origin: 'https://app.meetfranz.com',
+});
+
 const debug = require('debug')('Franz:Analytics');
 
 /* eslint-disable */
-const _paq = window._paq = window._paq || [];
+var _paq = window._paq = window._paq || [];
 
+_paq.push(["setCookieDomain", "app.meetfranz.com"]); 	
 _paq.push(['setCustomDimension', 1, app.getVersion()]);
+_paq.push(['setDomains', 'app.meetfranz.com']);
+_paq.push(['setCustomUrl', '/']);
 _paq.push(['trackPageView']);
-_paq.push(['enableLinkTracking']);
+
 
 (function() {
-  const u="https:////analytics.franzinfra.com/";
+  var u="https://analytics.franzinfra.com/";
   _paq.push(['setTrackerUrl', u+'matomo.php']);
   _paq.push(['setSiteId', '1']);
-  const d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
 /* eslint-enable */
 
-// const GA_LOCAL_STORAGE_KEY = 'gaUid';
-
-// ga((tracker) => {
-//   localStorage.setItem(GA_LOCAL_STORAGE_KEY, tracker.get('clientId'));
-// });
-// ga('set', 'checkProtocolTask', null);
-// ga('set', 'version', app.getVersion());
-
-// ga('send', 'App');
-
 export function gaPage(page) {
-  _paq.push(['trackPageView']);
-  debug('GA track page', page);
+  window._paq.push(['setCustomUrl', page]);
+  window._paq.push(['trackPageView']);
+
+  debug('Track page', page);
 }
 
 export function gaEvent(category, action, label) {
-  // ga('send', 'event', category, action, label);
-  _paq.push(['trackEvent', category, action, label]);
-  debug('GA track event', category, action, label);
+  window._paq.push(['trackEvent', category, action, label]);
+  debug('Track event', category, action, label);
 }
