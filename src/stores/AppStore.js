174160c53b6341f3547fb88fc380fd3b054296a0
Stefan Malzner <stefan@adlk.io>
2021-12-23 15:28:10 +0100

    test win autostart with app exe

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -341,9 +341,8 @@ export default class AppStore extends Store {
     this.autoLaunchOnStart = enable;
 
     try {
-      const appFolder = path.dirname(process.execPath);
-      const updateExe = path.resolve(appFolder, '..', 'Update.exe');
-      const exeName = path.basename(process.execPath);
+      const appExe = path.resolve(process.execPath);
+      const exeName = path.basename(appExe);
 
       const args = {
         openAtLogin: enable,
@@ -351,7 +350,7 @@ export default class AppStore extends Store {
 
       if (isWindows) {
         Object.assign(args, {
-          path: updateExe,
+          path: appExe,
           name: 'Franz',
           args: [
             '--processStart', `"${exeName}"`,
@@ -359,6 +358,8 @@ export default class AppStore extends Store {
           ],
         });
       }
+
+      debug('Setting login item settings to', args)
 
       app.setLoginItemSettings(args);
     } catch (err) {
